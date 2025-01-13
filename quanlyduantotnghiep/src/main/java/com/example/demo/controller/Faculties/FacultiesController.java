3daaa4c19e575fe3d9f5b6bc07333e4398f9f42e
VanDong <0769408945z@gmail.com>
2025-01-13 12:19:14 +0700

    Revert "Giao diện bảng Faculties, chỉ có thể hiển thị dữ liệu."
    This reverts commit ab6457a74cb03b5b226515cd4df29ffd93988da8.

--- quanlyduantotnghiep/src/main/java/com/example/demo/controller/Faculties/FacultiesController.java
+++ quanlyduantotnghiep/src/main/java/com/example/demo/controller/Faculties/FacultiesController.java
@@ -6,11 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,35 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.Faculties.Faculties;
 import com.example.demo.service.Faculties.FacultiesService;
 
-@Controller
+@RestController
 @RequestMapping("/faculties")
 public class FacultiesController {
 	
 	@Autowired
 	public FacultiesService facultiesService;
 	
-	 public FacultiesController(FacultiesService facultiesService) {
-	        this.facultiesService = facultiesService;
-	    }
-	@GetMapping("/view")
+	@GetMapping("")
 	  public ResponseEntity<List<Faculties>> getFaculties() {
         List<Faculties> faculties = this.facultiesService.getFaculties();
-       return ResponseEntity.ok(faculties);
+        return ResponseEntity.ok(faculties);
     }
-	 @GetMapping("")
-	    public String getFaculties(Model model) {
-//	        // Lấy danh sách faculties từ Service
-//	        List<Faculties> faculties = facultiesService.getFaculties();
-//	        // Đưa danh sách vào Model để truyền tới view
-//	        if(faculties.isEmpty()) {
-//	        	 model.addAttribute("message", "No patients found");
-//	        }
-//	        else {
-//	        	  model.addAttribute("faculties", faculties);
-//	        }
-//	        // Trả về file HTML tên là "faculties.html"
-	        return "faculties/form_faculties";
-	    }
 	
 //	@GetMapping("/{faId}")
 //	public Faculties getFaculties(@PathVariable long faId) {
@@ -67,16 +47,12 @@ public class FacultiesController {
 	
     
  // Thêm mới khoa
-//    @PostMapping("/save")
-//    public ResponseEntity<Faculties> addFaculties(@RequestBody Faculties fa) {
-//        Faculties faculties = this.facultiesService.addFaculties(fa);
-//        return ResponseEntity.status(HttpStatus.CREATED).body(faculties);
-//    }	
-    @PostMapping("/save")
-	  public String addFaculties(@ModelAttribute Faculties fa) {
-	      Faculties faculties = this.facultiesService.addFaculties(fa);
-	      return "redirect:/faculties";
-	  }	
+    @PostMapping("")
+    public ResponseEntity<Faculties> addFaculties(@RequestBody Faculties fa) {
+        Faculties faculties = this.facultiesService.addFaculties(fa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(faculties);
+    }
+	
     
  // Cập nhật thông tin khoa
     @PutMapping("/{faId}")
